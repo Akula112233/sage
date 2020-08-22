914e0c1320699f9523b765997c03b4d4f48f5f80
d0-rb <henryandrecastillo@gmail.com>
2020-08-22 16:59:29 -0500

    include build
    this way it is easier for me to add api

---
+++ build/precache-manifest.935abdc4661b31b2037860e7b78d4c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32919662a7f8caaed6dfc4d14d27068d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b4b192a0e9380d2b1b9",
+    "url": "/static/css/main.02ce3e77.chunk.css"
+  },
+  {
+    "revision": "d360a55feb2e6ebc32b4",
+    "url": "/static/js/2.7b8d258a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b8d258a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4b192a0e9380d2b1b9",
+    "url": "/static/js/main.8ede4cbd.chunk.js"
+  },
+  {
+    "revision": "71b8b8369569cc65570c",
+    "url": "/static/js/runtime-main.32f3ff5c.js"
+  }
+]);
