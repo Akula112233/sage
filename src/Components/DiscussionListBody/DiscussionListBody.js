4a8603ee37b42f279c7cd620f8b3f91fa2545680
Charan <phazia.co@gmail.com>
2020-08-22 12:36:34 -0500

    started discussion list body

---
+++ src/Components/DiscussionListBody/DiscussionListBody.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './DiscussionListBody.css'
+import TagsBody from './TagsBody/TagsBody'
+import DiscussionsNavBar from './DiscussionsNavBar/DiscussionsNavBar'
+import DiscussionsList from './DiscussionsList/DiscussionsList'
+
+export default class DiscussionListBody extends Component {
+    render() {
+        return (
+            <div id="discussion-list-body">
+                <DiscussionsNavBar></DiscussionsNavBar>
+                <div style={{height: "1px", width: "100%", backgroundColor: "#EAECF3"}}></div>
+                <TagsBody></TagsBody>
+            </div>
+        )
+    }
+}
